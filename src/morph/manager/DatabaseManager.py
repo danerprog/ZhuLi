bbf94523678a8836454a882bdca6ff42f28e3d5f
Daner Jimenez <danerprog@gmail.com>
2022-05-04 18:40:34 +0800

    removed VirtualDatabase until a proper implementation can be created

--- src/morph/manager/DatabaseManager.py
+++ src/morph/manager/DatabaseManager.py
@@ -10,8 +10,6 @@ class DatabaseManager:
         self._logger.debug("_initializeDatabase called. database_configuration: " + str(database_configuration))
         if database_configuration['program'] == 'mongo':
             self._initializeMongoDatabase(database_configuration)
-        elif database_configuration['program'] == 'virtual':
-            self._initializeVirtualDatabase(database_configuration)
         
     def _initializeMongoDatabase(self, params):
         self._logger.debug("_initializeMongoDatabase called. params: {}".format(str(params)))
@@ -19,12 +17,6 @@ class DatabaseManager:
         params['port'] = int(params['port'])
         params['logger'] = self._logger.getChild('MongoDatabase')
         self._database = MongoDatabase(**params)
-        
-    def _initializeVirtualDatabase(self, params):
-        self._logger.debug("_initializeVirtualDatabase called. params: {}".format(str(params)))
-        from morph.database.VirtualDatabase import VirtualDatabase
-        params['logger'] = self._logger.getChild('VirtualDatabase')
-        self._database = VirtualDatabase(**params)
 
     def get(self):
         return self._database
